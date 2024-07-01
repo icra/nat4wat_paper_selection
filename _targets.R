16385e7c4a4537db8573b28c1099a1c6c052410b
Josep Pueyo-Ros <jpueyo@icra.cat>
2024-07-01 16:36:48 +0200

    update renv.lock

--- _targets.R
+++ _targets.R
@@ -6,15 +6,13 @@ controller = crew::crew_controller_local(workers = 2, seconds_idle = 60)
 # Set target options:
 tar_option_set(
   # controller = controller,
-  packages = c("httr2", "tidytable", "stringr", "lhs", "purrr") # Packages that your targets need for their tasks.
+  packages = c("httr2", "tidytable", "stringr", "purrr") # Packages that your targets need for their tasks.
 )
 
-# Run the R scripts in the R/ folder with your custom functions:
 tar_source()
 n <- 1e5
 set.seed(1121)
 
-# Replace the target list below with your own:
 list(
   tar_target(inputs_treatment_raw, generate_inputs(wastewater, n)),
   tar_target(inputs_treatment, fix_household(inputs_treatment_raw)),
