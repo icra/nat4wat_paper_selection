1d4d78728d33ec397d62a73937759a607b4143cb
Josep Pueyo-Ros <jpueyo@icra.cat>
2024-10-03 16:49:37 +0200

    update pca plot

--- R/redundancy.R
+++ R/redundancy.R
@@ -4,7 +4,12 @@ plot_pca <- function(techs, file){
   
   df <- df_tr |> 
     select(id, vertical:inv_es_biohazard) |> 
-    select(-c(camping_wastewater, offices_wastewater)) |> 
+    select(
+      -c(camping_wastewater, offices_wastewater, rain_water, runoff_water), 
+      -contains("_dry"),
+      -starts_with("m2_pe"),
+      -starts_with("hlr_")
+    ) |> 
     tibble::as_tibble() |> 
     tibble::column_to_rownames("id")
   
@@ -35,11 +40,11 @@ plot_pca <- function(techs, file){
     ggplot(aes(x = Dim.1, y = Dim.2, label = rowname)) +
     geom_point(alpha = 0.2) +
     ggrepel::geom_text_repel(alpha = 0.2) +
-    geom_point(data = ind, mapping = aes(color = type), shape = 18) +
+    geom_point(data = ind, mapping = aes(color = type)) +
     ggrepel::geom_text_repel(data = ind, mapping = aes(color = type), show.legend = FALSE) +
     # scale_color_brewer(palette = "Paired", direction = -1) +
     scale_color_manual(values = rev(colors)) +
-    coord_cartesian(ylim = c(-5, 5)) +
+    coord_cartesian(ylim = c(-5, 4)) +
     labs(
       x = paste0("Dimension 1 (", round(pca_res$eig[1, 2], 2), "%)"),
       y = paste0("Dimension 2 (", round(pca_res$eig[2, 2], 2), "%)"),
